52919bfe9a53290fd0dd8b18c1e9c3691958170c
kiki le singe <aalbertini95@gmail.com>
2015-04-30 22:41:38 +0200

    chore(init): configure backbone.syphon path

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -33,6 +33,7 @@ require.config({
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.radio': '../bower_components/backbone.radio/build/backbone.radio',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+    'backbone.syphon': '../bower_components/marionette.backbone.syphon/lib/backbone.syphon',
 
     /* Alias text.js for template loading and shortcut the templates dir to tmpl */
     text: '../bower_components/requirejs-text/text',
