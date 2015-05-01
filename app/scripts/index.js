8a1eb2165e7401c9182a197a55e18f9f1c75c252
kiki le singe <aalbertini95@gmail.com>
2015-05-01 15:51:03 +0200

    refactor(index): removes adding socketio

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,11 @@
 require([
   'backbone',
-  'socketio',
   'app',
   'helpersHBS'
 ],
 
-function (Backbone, io, App) {
+function (Backbone, App) {
   'use strict';
-
-  var socket = io(); // jshint unused:false
 
   App.start();
   Backbone.history.start();
